6d85e9564dc3a70b4133820a2b8bb5483d5e32d2
Vignesh12345-hub <vignesh@gsstech.in>
2026-03-02 16:27:15 +0530

    Add files via upload
    added duplicates

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>() { 1, 2, 1, 6, 4, 2, 6, 3 };
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            for (int j = i + 1; j < numbers.Count; j++)
+            {
+                if (numbers[i] == numbers[j])
+                {
+                    numbers.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
+
+    
+
+
+
+
+
+
